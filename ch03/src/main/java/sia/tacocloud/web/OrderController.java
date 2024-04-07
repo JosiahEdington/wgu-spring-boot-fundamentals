bafbe88232783ea01a2ab4ae3370996fd9f3e958
Josiah Edington <josiah.edington@gmail.com>
2024-04-06 17:20:01 -0700

    Addressing OrderRepository in OrderController

--- ch03/src/main/java/sia/tacocloud/web/OrderController.java
+++ ch03/src/main/java/sia/tacocloud/web/OrderController.java
@@ -1,26 +1,30 @@
 package sia.tacocloud.web;
 
 import javax.validation.Valid;
-import org.springframework.validation.Errors;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import sia.tacocloud.TacoOrder;
+import sia.tacocloud.data.OrderRepository;
 
-
-@Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController { 
-    
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
@@ -36,7 +40,7 @@ public class OrderController {
             return "orderForm";
         }
         
-        log.info("Order submitted: {}", order); 
+        orderRepo.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
